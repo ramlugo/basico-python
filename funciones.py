574f9c4ea009f579884c1c54749ad49e542b054d
ramlugo <68721114+ramlugo@users.noreply.github.com>
2020-07-30 22:18:43 -0500

    Buenas prácticas en archivos de Python
    Porque puedo y estoy probando GitHub :3

--- funciones.py
+++ funciones.py
@@ -1,32 +1,37 @@
-# def imprimir_mensaje():
-#     print("Mensaje especial: ")
-#     print("Estoy aprendiendo a usar funciones")
+def run():
+    # def imprimir_mensaje():
+    #     print("Mensaje especial: ")
+    #     print("Estoy aprendiendo a usar funciones")
 
 
-# imprimir_mensaje()
-# imprimir_mensaje()
-# imprimir_mensaje()
+    # imprimir_mensaje()
+    # imprimir_mensaje()
+    # imprimir_mensaje()
 
-# def conversacion(mensaje):
-#     print("Hola")
-#     print("¿Cómo estás?")
-#     print(mensaje)
-#     print("Adiós")
+    # def conversacion(mensaje):
+    #     print("Hola")
+    #     print("¿Cómo estás?")
+    #     print(mensaje)
+    #     print("Adiós")
 
-# opcion = input("Elige una opcion (1, 2, 3): ")
-# if opcion == "1":
-#     conversacion("Elegiste la opción 1")
-# elif opcion == "2":
-#     conversacion("Elegiste la opción 2")
-# elif opcion == "3":
-#     conversacion("Elegiste la opción 3")
-# else:
-#     print("Escribe la opción correcta")
+    # opcion = input("Elige una opcion (1, 2, 3): ")
+    # if opcion == "1":
+    #     conversacion("Elegiste la opción 1")
+    # elif opcion == "2":
+    #     conversacion("Elegiste la opción 2")
+    # elif opcion == "3":
+    #     conversacion("Elegiste la opción 3")
+    # else:
+    #     print("Escribe la opción correcta")
 
-def suma(a, b):
-    print("Se suman dos números")
-    resultado = a + b
-    return resultado
+    def suma(a, b):
+        print("Se suman dos números")
+        resultado = a + b
+        return resultado
 
-sumatoria = suma (1, 4)
-print(sumatoria)
+    sumatoria = suma (1, 4)
+    print(sumatoria)
+
+
+if __name__ == "__main__":
+    run()
