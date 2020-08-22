d2b29e5758d10a8726709569791027bab6b8bb84
Ram Lugo <ramlugo2020@gmail.com>
2020-08-21 22:30:55 -0500

    Versión final del experimento

--- experimento.py
+++ experimento.py
@@ -1,7 +1,7 @@
 def run():
     name = input('Escribe tu nombre: ')
-    saludo = '¡Hola ' + name + ', el número de caracteres de este enunciado es ' + len(saludo)
-    print(saludo)
+    saludo = '¡Hola ' + name + '!, el número de caracteres de este enunciado es'
+    print(saludo, len(saludo))
 
 
 if __name__ == "__main__":
