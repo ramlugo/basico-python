af077aef54959d33db2ee0c68e814e0afae04372
Ram Lugo <ramlugo2020@gmail.com>
2020-08-21 22:25:58 -0500

    Nuevo archivo del curso Pensamiento Computacional en Python

---
+++ experimento.py
@@ -0,0 +1,8 @@
+def run():
+    name = input('Escribe tu nombre: ')
+    saludo = '¡Hola ' + name + ', el número de caracteres de este enunciado es ' + len(saludo)
+    print(saludo)
+
+
+if __name__ == "__main__":
+    run()
