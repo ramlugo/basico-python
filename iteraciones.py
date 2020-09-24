e19622748c820be33ef211992520ee3d8ad51129
Ram Lugo <ramlugo2020@gmail.com>
2020-09-23 22:16:19 -0500

    Primer uso de break en el curso

--- iteraciones.py
+++ iteraciones.py
@@ -1,9 +1,17 @@
 def run():
-    contador = 0
+    contador_externo = 0
+    contador_interno = 0
 
-    while contador < 10:
-        print(contador)
-        contador += 1 #contador = contador + 1
+    while contador_externo < 5:
+        while contador_interno < 6:
+            print(contador_externo ,contador_interno)
+            contador_interno += 1 #contador = contador + 1
+
+            if contador_interno >= 3:
+                break
+
+        contador_externo += 1
+        contador_interno = 0
 
 
 if __name__ == "__main__":
