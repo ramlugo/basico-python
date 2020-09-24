3fefd4ab40a8da59c10548c3bd1bf7bf6187bb9a
Ram Lugo <ramlugo2020@gmail.com>
2020-09-23 22:22:05 -0500

    Funcionamiento de un reloj en el proyecto de iteraciones

--- iteraciones.py
+++ iteraciones.py
@@ -2,12 +2,12 @@ def run():
     contador_externo = 0
     contador_interno = 0
 
-    while contador_externo < 5:
-        while contador_interno < 6:
+    while contador_externo < 12:
+        while contador_interno < 12:
             print(contador_externo ,contador_interno)
             contador_interno += 1 #contador = contador + 1
 
-            if contador_interno >= 3:
+            if contador_interno >= 6:
                 break
 
         contador_externo += 1
