4218e71a03f02d2f8a691091882ca24577e96514
Ram Lugo <ramlugo2020@gmail.com>
2020-10-12 20:58:17 -0500

    Programa terminado

--- numeracion.py
+++ numeracion.py
@@ -1,5 +1,16 @@
 def run():
+    obejtivo = int(input("Escoge un entero: "))
+    respuesta = 0
+
+
+    while respuesta**2 < obejtivo:
+        respuesta+= 1
     
+
+    if respuesta**2 == obejtivo:
+        print(f"La raíz cuadrada de {obejtivo} es {respuesta}")
+    else:
+        print(f"{obejtivo} no tiene raíz cuadrada exacta")
 
 
 if __name__ == "__main__":
